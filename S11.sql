cf045b3097095474b4f298af423c9de1f2eed9e7
Dany <148161604+Cristhian036@users.noreply.github.com>
2023-11-02 08:27:11 -0500

    Maquinaria.sql

--- S11.sql
+++ S11.sql
@@ -94,7 +94,7 @@ SELECT*FROM Asignacion
 --   HAVING     --
 ------------------
 
---Haga una consulta que muestre el total del costo referencial de los proyectos de Cerro Azul y Arcata
+--1. Haga una consulta que muestre el total del costo referencial de los proyectos de Cerro Azul y Arcata
 	--group by
 	SELECT ubicacion, sum(costo_referencial) FROM MAQUINARIA
 	WHERE ubicacion in('Cerro Azul','Arcata')
@@ -105,38 +105,67 @@ SELECT*FROM Asignacion
 	GROUP BY ubicacion
 	having ubicacion in('Arcata','Cerro Azul')
 
---Haga una consulta que muestre el monto total de maquinaria por proyecto, excluya de esa relacion aquellos montos totales que no superen los 90000 soles
+--2. Haga una consulta que muestre el monto total de maquinaria por proyecto, excluya de esa relacion aquellos montos totales que no superen los 100000 soles
 	SELECT ubicacion,sum(costo_referencial) from MAQUINARIA
 	group by ubicacion	
 	having sum(costo_referencial)>90000
 	
---Haga una consulta que muestre el monto total de maquinaria por proyecto, excluya a la escavadora m2 y de esa relacion aquellos montos totales que no superen los 90000 soles
+--2.1 Haga una consulta que muestre el monto total de maquinaria por proyecto, excluya a la escavadora m2 y de esa relacion aquellos montos totales que no superen los 90000 soles
 	SELECT ubicacion,sum(costo_referencial) from MAQUINARIA
 	where descripcion != 'Escavadora M2'
 	group by ubicacion	
 	having sum(costo_referencial)>90000
 	--order by
 
-
---Haga una consulta que muestre en que puestos se asignaron a mas de 3 personas.
+--3. Haga una consulta que muestre en que puestos se asignaron a mas de 3 personas.
 	SELECT puesto,count(puesto) from Personal
 	group by puesto
 	having count(puesto)>2
 
---Haga la consulta 3. Pero excluya al puesto Operador de esta relación	
+-- 4. Haga la consulta 3. Pero excluya al puesto Operador de esta relación
 	SELECT puesto,count(puesto) from Personal
 	where puesto != 'Operador'
 	group by puesto
 	having count(puesto)>2
 
---Haga una consulta que muestre que maquinas tienen mas de 2 personas asignadas
+--5. Haga una consulta que muestre que maquinas tienen mas de 2 personas asignadas
 	SELECT MAQUINARIA.descripcion, COUNT(Asignacion.cod_personal)from MAQUINARIA INNER JOIN Asignacion
 	ON MAQUINARIA.cod_maq=Asignacion.cod_maq
 	GROUP BY MAQUINARIA.descripcion
 
+--6. Muestre cuanto es el total por Escavadoras y Perforadoras. Excluya de la relacion a la Escavadora 700
+	SELECT descripcion, sum(costo_referencial)
+	FROM MAQUINARIA
+	WHERE descripcion != 'Escavadora 700' and (descripcion like 'Escavadora%' or descripcion like 'Perforadora%')
+	group by descripcion
+
+	Select * from MAQUINARIA
+
 ----------------------------------
---		TOP SUBCONSULTAS		--
+--		TOP Y SUBCONSULTAS		--
 ----------------------------------
+
+--maquinarias por encima del promedio total
+	Select *from MAQUINARIA
+	where costo_referencial >(SELECT AVG(costo_referencial) FROM MAQUINARIA )
+	SELECT AVG(costo_referencial) FROM MAQUINARIA
+
+--Mostrar maquinarias cuyo costo_referencial este entre el menor de los costos y el promedio
+	Select *from MAQUINARIA
+	where costo_referencial between (SELECT min(costo_referencial) FROM MAQUINARIA	) and (SELECT AVG(costo_referencial) FROM MAQUINARIA )
+
+	SELECT min(costo_referencial) FROM MAQUINARIA
+	SELECT AVG(costo_referencial) FROM MAQUINARIA
+
+--Mostrar el segundo costo mas bajo
+	SELECT TOP 1 * from MAQUINARIA 
+	where cod_maq IN 
+	(SELECT TOP 2 cod_maq FROM MAQUINARIA order by costo_referencial asc) 
+	order by costo_referencial desc
+
+	SELECT * FROM MAQUINARIA
+	WHERE exists (Select * from MAQUINARIA
+				where  costo_referencial > 50000)
 
 --1. Diga cual es el proyecto con costo referencial mas bajo y que no tenga personal asignado a sus maquinarias
 --primero por partes
@@ -158,10 +187,26 @@ SELECT*FROM Asignacion
 --4. Muestre el nombre del personal que esta asignado a la penultima maquinaría con costo mas bajo
 
 --5. Muestre los 2 primeros nombres del personal que no seas perforistas asignados a las maquinarias de Ares con mayor costo
-	SELECT Personal.nombres, Personal.apellidopat,Personal.puesto,MAQUINARIA.descripcion,MAQUINARIA.costo_referencial 
+	--sin sub consulta
+	SELECT Personal.nombres, Personal.apellidopat,Personal.puesto,MAQUINARIA.descripcion,MAQUINARIA.ubicacion,MAQUINARIA.costo_referencial 
 	FROM MAQUINARIA inner join Asignacion 
 	on MAQUINARIA.cod_maq=Asignacion.cod_maq inner join Personal 
 	on Asignacion.cod_personal=Personal.cod_personal
+	where (Personal.puesto !='Perforista' and MAQUINARIA.ubicacion!='Ares')
+	order by costo_referencial desc
+
+	--sub consulta
+	SELECT Personal.nombres, Personal.apellidopat,Personal.puesto,MAQUINARIA.descripcion,MAQUINARIA.ubicacion,MAQUINARIA.costo_referencial 
+	FROM MAQUINARIA inner join Asignacion 
+	on MAQUINARIA.cod_maq=Asignacion.cod_maq inner join Personal 
+	on Asignacion.cod_personal=Personal.cod_personal
+	where Personal.puesto != 'Perforista' and Maquinaria.ubicacion IN (SELECT Personal.nombres, Personal.apellidopat,Personal.puesto,MAQUINARIA.descripcion,MAQUINARIA.ubicacion,MAQUINARIA.costo_referencial 
+												FROM MAQUINARIA inner join Asignacion 
+												on MAQUINARIA.cod_maq=Asignacion.cod_maq inner join Personal 
+												on Asignacion.cod_personal=Personal.cod_personal
+												Where MAQUINARIA.ubicacion != 'Ares')
+
+
 	where (Personal.puesto !='Perforista' and MAQUINARIA.ubicacion!='Ares')
 	order by costo_referencial desc
 
